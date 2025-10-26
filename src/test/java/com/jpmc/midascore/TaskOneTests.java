6a2f33e81b51b18e13bdd739c1977dfc8bc59888
lakshitachandrakar <lakshitachandrakar@gmail.com>
2025-10-27 01:20:47 +0530

    chore: add necessary dependencies
    fix: correct test code for proper execution

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -4,10 +4,37 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
+
+    /*
+    Initially, spring tred to start the application context, however it failed, because it was trying to instantiate KafkaProducer
+    To rectify it, we initially tried to mock KafkaProducer by doing
+        @Mock
+        private KafkaProducer kafkaProducer;
+
+       however this failed, because KafkaProducer contains final feild, and no-args constructor, leading to MockitoException
+
+       Thus, instead of mocking we created a test configuration bean that Spring can inject. This beans will do nothing,
+       it is created just for the purpose of testing.
+     */
+    @TestConfiguration
+    static class KafkaProducerTestConfig {
+        @Bean
+        public KafkaProducer kafkaProducer() {
+            // return a dummy implementation that does nothing
+            return new KafkaProducer("test-topic", null) {
+                @Override
+                public void send(String transactionLine) {
+                    // do nothing
+                }
+            };
+        }
+    }
 
     @Test
     void task_one_verifier() throws InterruptedException {
